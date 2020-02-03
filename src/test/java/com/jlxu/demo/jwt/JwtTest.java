ed5c4de3a10e09014bc8ef98d1c1830259d51d02
jlxu <xujinli0916@163.com>
2020-02-03 21:18:59 +0800

    TODO:Jwt测试

---
+++ src/test/java/com/jlxu/demo/jwt/JwtTest.java
@@ -0,0 +1,14 @@
+package com.jlxu.demo.jwt;
+
+import com.jlxu.demo.jwt.utils.JwtUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JwtTest {
+
+    @Test//测试生成jwt json token
+    public void test() {
+
+    }
+}
