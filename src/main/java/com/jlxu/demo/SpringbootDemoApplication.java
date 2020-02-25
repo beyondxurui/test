40012fcad546a045fb70a5dbf35128a27274f195
jlxu <xujinli0916@163.com>
2020-02-25 21:47:15 +0800

    springboot集成activiti未完（如何建立bpm文件）

--- src/main/java/com/jlxu/demo/SpringbootDemoApplication.java
+++ src/main/java/com/jlxu/demo/SpringbootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//        (exclude = {org.activiti.spring.boot.SecurityAutoConfiguration.class})
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
-
 }
