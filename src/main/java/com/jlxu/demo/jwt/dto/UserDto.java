75791cc0bc59fe7b8324d7298ce382f4a9177d53
jlxu <xujinli0916@163.com>
2020-04-22 09:46:27 +0800

    推送一个空的项目

--- src/main/java/com/jlxu/demo/jwt/dto/UserDto.java
+++
@@ -1,8 +0,0 @@
-package com.jlxu.demo.jwt.dto;
-
-import com.jlxu.demo.jwt.model.User;
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = false)
-public class UserDto extends User {
-}
